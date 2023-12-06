bd9cb42a815014343b3138b8dcceb5ddee9b1baa
Costas Ioannou <ioannou.dev@gmail.com>
2023-12-06 18:25:10 +0200

    ugly character list in page

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,10 @@
 // noinspection JSUnusedGlobalSymbols
-
+import 'reflect-metadata';
 import { Stack } from 'expo-router';
 import { configure } from 'mobx';
+
+import { container } from '~config/ioc/AppIOC';
+import { InjectionProvider } from '~config/ioc/injection.react';
 
 configure({
   enforceActions: 'always',
@@ -10,5 +13,9 @@ configure({
   observableRequiresReaction: true,
 });
 export default function HomeLayout() {
-  return <Stack />;
+  return (
+    <InjectionProvider container={container}>
+      <Stack />
+    </InjectionProvider>
+  );
 }
